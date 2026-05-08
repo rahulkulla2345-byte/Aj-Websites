bf4d5ca9f8e3a21835101e3dfdaa98f5c307b611
Arjun <arjun@Mac-mini.local>
2026-05-08 19:51:40 +0530

    force railway chrome channel

--- extractor.py
+++ extractor.py
@@ -28,6 +28,7 @@ async def scrape_site_async(base_url):
     async with async_playwright() as playwright:
 
         browser = await playwright.chromium.launch(
+            channel="chrome",
             headless=True,
             args=[
                 "--no-sandbox",
